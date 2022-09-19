4d3aea2b29302e2be3861f85e2a62e7a254d5430
DubrovinMikhail <mixailgoo@gmail.com>
2022-09-19 22:54:58 +0300

    added files

---
+++ m2m-relations/articles/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+
+
+from .models import Article, Scope, Tag
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            if form.cleaned_data['is_main']:
+                count += 1
+                form.cleaned_data
+        if count > 1:
+            raise ValidationError('Основной тег должен быть один!')
+
+        if count < 1:
+            raise ValidationError('Выберете один основной тег!')
+        return super().clean()
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+    extra = 0
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    # list_display = ['id', 'title' ]
+    inlines = [ScopeInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    # list_display = ['id', 'name']
+    pass
